b8e888b925a5dbf073eb052b970af591133ed5a4
tofuwine <tofuwine@outlook.com>
2024-05-28 09:19:31 +0800

    修正无目录时，js 问题

--- assets/js/pe-toc.js
+++ assets/js/pe-toc.js
@@ -2,7 +2,12 @@
 let activatedTitle;
 let articleTitles = [];
 window.addEventListener('DOMContentLoaded', function (event) {
-    checkTocPosition();
+    const tocContainer = document.getElementById("toc-container");
+    if (!tocContainer) {
+        return;
+    }
+
+    checkTocPosition(tocContainer);
 
     articleTitles = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]');
     // Make the first header active
@@ -39,13 +44,13 @@ const main = parseInt(getComputedStyle(document.body).getPropertyValue('--articl
 const toc = parseInt(getComputedStyle(document.body).getPropertyValue('--toc-width'), 10);
 const gap = parseInt(getComputedStyle(document.body).getPropertyValue('--gap'), 10);
 
-function checkTocPosition() {
+function checkTocPosition(tocContainer) {
     const width = document.body.scrollWidth;
 
     if (width - main - (toc * 2) - (gap * 4) > 0) {
-        document.getElementById("toc-container").classList.add("wide");
+        tocContainer.classList.add("wide");
     } else {
-        document.getElementById("toc-container").classList.remove("wide");
+        tocContainer.classList.remove("wide");
     }
 }
 
